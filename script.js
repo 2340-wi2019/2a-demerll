1d950b3cd4fe622cb0b202897903787e7705cade
demerll <58672347+demerll@users.noreply.github.com>
2019-12-15 20:22:29 -0600

    Completed Assignment

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+    /*
+		Donjeta Emerllahu
+		INFO2340
+		12/2019
+
+    */
+
+    $('#female_1_1').html;
+    var femaleName= prompt("Please enter a females firt name? (1 of 2)")
+    var femaleName= prompt("Please enter a females firt name? (2 of 2)");
+
+    $('#male_1_1')
+    var maleName= prompt("Please enter a males firt name?");
+
+    $('#state_1_1')
+    var stateName= prompt("Please enter a name of a state?");
+
+
+    $('#geo_1_1')
+    var geoName= prompt("Please enter a name of a geographical feature?");
+
+    $('#occupation_1_1')
+    var occupationName= prompt("Please enter a name of an occupation");
+
+    $('#noun_1_1')
+    var aNoun= prompt("Please enter a name of a noun (1 of 5)")
+    var aNoun= prompt("Please enter a name of a noun (2 of 5)")
+    var aNoun= prompt("Please enter a name of a noun (3 of 5)")
+    var aNoun= prompt("Please enter a name of a noun (4 of 5)")
+    var aNoun= prompt("Please enter a name of a noun (5 of 5)");
+
+    $('#color_1_1');
+    var aColor= prompt("Please enter a color");
+
+
+});
